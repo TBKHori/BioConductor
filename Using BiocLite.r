6f0a5ddab3c325cac546d5715c19970240b57866
Telvin Roymen <tmaromeo5@gmail.com>
2020-11-17 00:37:33 +0300

    2nd Commit

--- Using BiocLite.r
+++ Using BiocLite.r
@@ -58,3 +58,60 @@ packageVersion("BSgenome")
 
 # Negative
 # Bad at validating types and naming conventions
+# Inherritance works, but depends on the input
+
+# S4
+# Positive
+# Formal defination of classes
+# Bioconductor reusability
+# Has validation of types
+# Naming conventions
+# Example: mydescriptor <- new("GenomeDescription")
+
+# Negative
+# Complex structure compared to S3
+
+# Is it S4 or not?
+# Ask if an object is S4
+isS4(mydescriptor)
+# Result: TRUE
+
+str of S4 objects start with Formal classes
+
+str(mydescriptor)
+# Result: Formal class 'GenomeDescription' [package "GemeInfoDb"] with 7 slots
+...
+
+# S4 class definition
+# A class describes a representation
+
+# name
+# solts (methods/fields)
+# contains (inheritance definiton)
+
+MyEpicProject <- setClass(# Define class name with UpperCamelCase
+                        "MyEpicProject",
+                        # Define slots, helpful for validation
+                        solts c(ini = "Date",
+                                end = "Date"
+                                milestone = "character"),
+                        # Define inheritance
+                        contains = "MyProject")
+
+.s4methods(class= "GenomeDescription")
+# [1] commonName organism provider providerVersion releaseDate releaseName seqinfo
+# [8] seqnames show toString bsgenomeName
+
+showMethods(classes = "GenomicDescription", where = search())
+
+# Object summary
+show(myDescriptor)
+
+# Result:
+# | organism: ()
+# | provide:
+# | provider version:
+# | release date:
+# | release name:
+# | ---
+# | seqlenghts:
